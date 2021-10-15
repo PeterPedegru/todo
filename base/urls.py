46315d074bbf477ea43531dc7132d5cf2c59a24b
krutoneka <krutoneks3@gmail.com>
2021-10-15 17:19:21 +0300

    step_2

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteTask
 
 urlpatterns = [
-    path('', views.taskList, name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', DeleteTask.as_view(), name='task-delete'),
 ]
